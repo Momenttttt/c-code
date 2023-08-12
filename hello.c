d8c7a24d00a14e98be364dfa5c9d2da42222a5c4
1254345012 <1254345012@qq.com>
2023-08-12 16:48:13 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,13 +5,8 @@ void printHello()
 }
 int main()
 {
-<<<<<<< HEAD
     int a,b;
     scanf("%d %d",&a,&b);
     printf("%d\n",a+b);
-=======
-    printf("hello world!\n");
-    printHello();
->>>>>>> 636fbf6652acf065e8b8dc01c428933690f294f4
     return 0;
 }
